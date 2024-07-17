4502fb1e3f08153787193d1ca8f9ea6620080a73
Marianna Milovanova <mmilovanova@kse.org.ua>
2024-07-17 12:15:14 -0300

    fix: commit dist

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare function isValidQueryValue(value: any): boolean;
+export declare function addQueryParam(urlObj: URL, key: string, value: any): void;
+export declare function addQueryToUrl(url: string, query: Record<string, any>): string;
