a01e1eaa4f1c23cbf16dd4e49ac9a5a98e2f0a11
Marianna Milovanova <mmilovanova@kse.org.ua>
2024-07-17 13:00:49 -0300

    fix: try with lib

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function isValidQueryValue(value: any): boolean;
-export declare function addQueryParam(urlObj: URL, key: string, value: any): void;
-export declare function addQueryToUrl(url: string, query: Record<string, any>): string;
